c46278178ba41ff81103358e74e4cd211988de02
Alex <alex@10.0.1.89>
2013-09-28 16:30:17 -0700

    Initial Import - Added User Variables - Started UI

---
+++ StatMeter/src/com/techtalk4geeks/blogspot/User.java
@@ -0,0 +1,29 @@
+package com.techtalk4geeks.blogspot;
+
+public class User
+{
+String myName = "User.getName()";
+int myLevel = 1;
+int myRank = 1;
+int myBirthYear;
+int myHP;
+int mySP;
+int myPOW;
+int myDEF;
+int mySPEED;
+
+	public User(String name, int birthYear, int SP) {
+		myName = name;
+		myBirthYear = birthYear;
+		mySP = SP;
+	}
+	
+	public String getName() {
+		return myName;
+	}
+	
+	public int getLevel() {
+		return myLevel;
+	}
+	
+}
