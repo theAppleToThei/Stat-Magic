2f7fb6e49c793d3af62ff183de930dc9b7681e46
Alex <alex@10.0.1.89>
2013-10-19 15:11:48 -0700

    - Fixed Bugs

--- StatMeter/src/com/techtalk4geeks/blogspot/User.java
+++ StatMeter/src/com/techtalk4geeks/blogspot/User.java
@@ -1,14 +1,14 @@
 package com.techtalk4geeks.blogspot;
 
 enum Rank {
-	LILYPAD, FLY, TADPOLE, KOI, FROG
+	LILYPAD, LEECH, FLY, POND_SKATER, POND_SNAIL, TADPOLE, KOI, FROG, DUCK, TURTLE
 }
 
 public class User
 {
 String myName = "User.getName()";
 int myLevel = 1;
-int myRank = 1;
+Rank myRank;
 int myBirthYear;
 int myHP;
 int mySP;
@@ -16,9 +16,10 @@ int myPOW;
 int myDEF;
 int mySPEED;
 
-	public User(String name, int birthYear, int SP) {
+	public User(String name, Rank rank, int birthYear, int SP) {
 		myName = name;
 		myBirthYear = birthYear;
+		myRank = rank;
 		mySP = SP;
 	}
 	
@@ -29,7 +30,4 @@ int mySPEED;
 	public int getLevel() {
 		return myLevel;
 	}
-	
-
-	
 }
