12fc155a042173571f1193fad6f45c17f2c18001
Alex Baratti <alex.techtalk4geeks@gmail.com>
2014-03-29 15:07:06 -0700

    Added Example to Hometown Label

---
+++ StatMagic/src/com/techtalk4geeks/blogspot/BattleActivity.java
@@ -0,0 +1,46 @@
+package com.techtalk4geeks.blogspot;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class BattleActivity extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.dev_battle);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.battle_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		// Handle item selection
+		switch (item.getItemId())
+		{
+		case R.id.attack:
+			return true;
+		case R.id.special:
+			return true;
+		case R.id.inventorybattle:
+			return true;
+		case R.id.defend:
+			return true;
+		case R.id.flee:
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+}
