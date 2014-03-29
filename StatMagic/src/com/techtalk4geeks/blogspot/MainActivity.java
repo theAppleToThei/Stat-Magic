87488a78d951973f6e09feccf4d3e72864e85501
Alex Baratti <alex.techtalk4geeks@gmail.com>
2014-03-29 15:11:49 -0700

    Added Link to Battle Activity using menu bar and battle button

--- StatMagic/src/com/techtalk4geeks/blogspot/MainActivity.java
+++ StatMagic/src/com/techtalk4geeks/blogspot/MainActivity.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Map;
 
 import android.content.Context;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -334,7 +335,8 @@ public class MainActivity extends FragmentActivity implements LocationListener
 			isCard = false;
 			return true;
 		case R.id.battle:
-			setContentView(R.layout.dev_battle);
+			Intent battle = new Intent(this, BattleActivity.class);
+			startActivity(battle);
 			isCard = false;
 			isBattle = true;
 			return true;
